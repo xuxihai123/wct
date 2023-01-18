16e90512b4e97f37154526ceebfefb8ff4984fc2
xuxihai <xuxihai@csii.com.cn>
2023-01-18 15:06:38 +0800

    remove come info

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
-const WebApiBench = require('./');
+const WebApiBench = require("./");
 
-const fs = require('fs');
-
+const fs = require("fs");
 
 var workbench = new WebApiBench({
-//   url: "http://127.0.0.1:8888/",
-url: "http://115.182.90.205:30344/a11y/text2speech?speed=50&text=abcdefghahfsf&volume=100&access_token=9527c6704e5945798231b80775e36c64&vendor=iflytek&channelId=pweb",
-
+  url: "http://127.0.0.1:8888/",
   vuser: 50,
   timeSpace: 2,
   runSecs: 5 * 60,
@@ -14,8 +11,8 @@ url: "http://115.182.90.205:30344/a11y/text2speech?speed=50&text=abcdefghahfsf&v
   slowStartTime: 60,
 });
 
-workbench.on('report',function(data){
-    fs.writeFileSync("./report/chartdata.json",JSON.stringify(data,null,2));
-})
+workbench.on("report", function (data) {
+  fs.writeFileSync("./report/chartdata.json", JSON.stringify(data, null, 2));
+});
 
 workbench.start();
